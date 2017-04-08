d0b5b9e303ea56659acfb0e17e68ab9a344ccb44
jasontruong <jasontruong91@gmail.com>
2017-04-08 18:53:35 +0800

    Add get param from url. Automate the process, read from setup.json, write to /output/app_x_login_x.json Currently only works on app_0_login_1.json others will need to improve login method

--- webcrawler/webcrawler/main.py
+++ webcrawler/webcrawler/main.py
@@ -1,2 +1,22 @@
-from scrapy import cmdline
-cmdline.execute("scrapy crawl web -o stored.json".split())
+import os
+import json
+
+dir_path = os.path.dirname(os.path.realpath(__file__)).replace(' ', '\ ') # web-security-scanner/webcrawler/webcrawler
+output_path = '%s/output/' % dir_path
+command_tml = 'scrapy crawl web -a app_index=%s -a login_index=%s -o '
+file_name_tml = 'app_%s_login_%s.json'
+
+# clear output folder
+os.system("rm -r %s" % output_path)
+
+# data is taken from setup.json
+with open('setup.json') as setup_file:
+    data = json.load(setup_file)
+for app_index in range(0, len(data)):
+    for login_index in range(0, len(data[app_index]['logins'])):
+        file_name = file_name_tml % (app_index, login_index)
+        output_file_uri = output_path + file_name
+
+        command = command_tml % (app_index, login_index)
+        command += output_file_uri
+        os.system(command)
